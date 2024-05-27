e39e6932ce5ac32be4100e61d7e18671d3b64895
YsnUth <79665934+YsnKey@users.noreply.github.com>
2024-05-27 08:33:46 -0500

    better

--- src/main/java/me/alpha432/oyvey/features/modules/oyveydotconfirm/Backup.java
+++ src/main/java/me/alpha432/oyvey/features/modules/oyveydotconfirm/Backup.java
@@ -9,16 +9,7 @@ public class Backup extends Module{
     public Backup() {
         super("Backupcaller", "", Module.Category.OYVEYDOTCONFIRM, true, false, false);
     }
-    private double x;
-    private double y;
-    private double z;
-    private double coords;
-    public void updatePosition() {
-        this.x = mc.player.getX();
-        this.y = mc.player.getY();
-        this.z = mc.player.getZ();
-        this.coords = mc.player.getX(); mc.player.getY(); mc.player.getZ();
-    }
+
     @Override
     public void onEnable(){
         ChatUtil.serverSendMessage("I need Backup " + mc.player.getX() + " " + mc.player.getY() + " " + mc.player.getZ());
