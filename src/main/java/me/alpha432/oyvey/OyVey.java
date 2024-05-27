98e16dc878b00e762bd82c5b258b9ff6c6337a53
YsnUth <79665934+YsnKey@users.noreply.github.com>
2024-05-27 11:58:06 -0500

    new version everytime :3

--- src/main/java/me/alpha432/oyvey/OyVey.java
+++ src/main/java/me/alpha432/oyvey/OyVey.java
@@ -8,7 +8,7 @@ import org.apache.logging.log4j.Logger;
 
 public class OyVey implements ModInitializer, ClientModInitializer {
     public static final String NAME = "Oyvey";
-    public static final String VERSION = "v2.13.5-extern+274.ba4c68c147";
+    public static final String VERSION = "0.3";
 
     public static float TIMER = 1f;
 
