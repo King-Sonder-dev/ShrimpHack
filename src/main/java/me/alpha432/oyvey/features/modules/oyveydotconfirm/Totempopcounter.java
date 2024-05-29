a8d89d8e0702a3b301ad98d07f59b07b837240e5
YsnUth <79665934+YsnKey@users.noreply.github.com>
2024-05-28 23:21:55 -0500

    ported the 1.12.2 hopin it would work prob not

--- src/main/java/me/alpha432/oyvey/features/modules/oyveydotconfirm/Totempopcounter.java
+++ src/main/java/me/alpha432/oyvey/features/modules/oyveydotconfirm/Totempopcounter.java
@@ -1,26 +1,66 @@
 package me.alpha432.oyvey.features.modules.oyveydotconfirm;
 
 import me.alpha432.oyvey.event.impl.TotemPopEvent;
+import me.alpha432.oyvey.features.Feature;
+import me.alpha432.oyvey.features.commands.Command;
 import me.alpha432.oyvey.features.modules.Module;
+import net.minecraft.util.Formatting;
 import me.alpha432.oyvey.util.ChatUtil;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.entity.player.PlayerEntity;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
 import java.util.Objects;
 
 public class Totempopcounter extends Module {
     private int popCounter;
+    public static HashMap<String, Integer> TotemPopContainer = new HashMap();
 
     public Totempopcounter() {
         super("TotemPopCounter", "", Module.Category.OYVEYDOTCONFIRM, true, false, false);
     }
 
-    // event handler (idk what the exact event name is)
-    @EventHandler
-    private void popEvent(TotemPopEvent event) {
-        popCounter++;
-        ChatUtil.clientSendMessage("I just popped " + event.entity());
-    };
-}
 
+
+
+    @Override
+    public void onEnable() {
+        TotemPopContainer.clear();
+    }
+
+    public void onDeath(PlayerEntity player) {
+        if (TotemPopContainer.containsKey(player.getName())) {
+            int l_Count = TotemPopContainer.get(player.getName());
+            TotemPopContainer.remove(player.getName());
+            if (l_Count == 1) {
+                Command.sendMessage(player.getName() + " died after popping " +  l_Count + " Totem!");
+            } else {
+                Command.sendMessage(player.getName() + " died after popping " +  l_Count + " Totems!");
+            }
+        }
+    }
+    public static boolean fullNullCheck() {
+        return Feature.mc.player == null || Feature.mc.world == null;
+    }
+    public void onTotemPop(PlayerEntity player) {
+        if (fullNullCheck()) {
+            return;
+        }
+        if (mc.player.equals(player)) {
+            return;
+        }
+        int l_Count = 1;
+        if (TotemPopContainer.containsKey(player.getName())) {
+            l_Count = TotemPopContainer.get(player.getName());
+            TotemPopContainer.put(String.valueOf(player.getName()), ++l_Count);
+        } else {
+            TotemPopContainer.put(String.valueOf(player.getName()), l_Count);
+        }
+        if (l_Count == 1) {
+            Command.sendMessage(player.getName() + " popped " + l_Count + " Totem.");
+        } else {
+            Command.sendMessage(player.getName() + " popped " + l_Count + " Totems.");
+        }
+    }
+}
