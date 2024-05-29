ab3effb39c4503e225af337c5d9098a9e79fed22
YsnUth <79665934+YsnKey@users.noreply.github.com>
2024-05-28 22:52:04 -0500

    new module

--- src/main/java/me/alpha432/oyvey/features/modules/oyveydotconfirm/Totempopcounter.java
+++ src/main/java/me/alpha432/oyvey/features/modules/oyveydotconfirm/Totempopcounter.java
@@ -2,6 +2,7 @@ package me.alpha432.oyvey.features.modules.oyveydotconfirm;
 
 import me.alpha432.oyvey.event.impl.TotemPopEvent;
 import me.alpha432.oyvey.features.modules.Module;
+import me.alpha432.oyvey.util.ChatUtil;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.entity.player.PlayerEntity;
 import org.jetbrains.annotations.NotNull;
@@ -9,8 +10,17 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Objects;
 
 public class Totempopcounter extends Module {
+    private int popCounter;
+
     public Totempopcounter() {
         super("TotemPopCounter", "", Module.Category.OYVEYDOTCONFIRM, true, false, false);
     }
 
+    // event handler (idk what the exact event name is)
+    @EventHandler
+    private void popEvent(TotemPopEvent event) {
+        popCounter++;
+        ChatUtil.clientSendMessage("I just popped " + event.entity());
+    };
 }
+
--- src/main/java/me/alpha432/oyvey/manager/ModuleManager.java
+++ src/main/java/me/alpha432/oyvey/manager/ModuleManager.java
@@ -38,6 +38,7 @@ public class ModuleManager implements Jsonable, Util {
         modules.add(new TPSChecker());
         modules.add(new SpeedHud());
         modules.add(new randomhud());
+        modules.add(new Totempopcounter());
         modules.add(new HitboxDesyncModule());
         modules.add(new Backup());
         modules.add(new AutoMeow());
