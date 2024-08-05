550dbb44d5c60071aa641ae0efc14a48850b61ce
YsnUth <79665934+YsnKey@users.noreply.github.com>
2024-08-05 15:33:51 -0500

    moreutils

--- src/main/java/me/alpha432/oyvey/util/CombatUtil.java
+++ src/main/java/me/alpha432/oyvey/util/CombatUtil.java
@@ -1,12 +1,16 @@
 package me.alpha432.oyvey.util;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.math.BlockPos;
 
 import static me.alpha432.oyvey.util.BlockUtil.getState;
 
 public class CombatUtil {
+    public static BlockPos modifyPos;
+    public static BlockState modifyBlockState = Blocks.AIR.getDefaultState();
+
     public static boolean isHard(BlockPos pos) {
         Block block = getState(pos).getBlock();
         return block == Blocks.OBSIDIAN || block == Blocks.NETHERITE_BLOCK || block == Blocks.ENDER_CHEST || block == Blocks.BEDROCK || block == Blocks.ANVIL;
