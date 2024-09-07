f0a6ae25ebfb46f5eaf0ac39234e31f9672f789e
YsnUth <79665934+YsnKey@users.noreply.github.com>
2024-09-07 08:43:17 -0500

    UPDATED TICKRATE

--- src/main/java/aids/dev/shrimphack/features/modules/client/TickRateHUD.java
+++ src/main/java/aids/dev/shrimphack/features/modules/client/TickRateHUD.java
@@ -9,24 +9,26 @@ import java.util.Random;
 public class TickRateHUD extends Module {
     private final Setting<Integer> yCoords = new Setting<>("Ycoords", 2, 0, 485);
     private final Setting<Integer> xCoords = new Setting<>("Xcoords", 2, 0, 710);
+
     public TickRateHUD() {
         super("TickRateHUD", "", Category.CLIENT, true, false, false);
     }
+
     private final Random random = new Random();
     private double lastTickRate = 20.0;
     private double lastChange = 0.0;
 
     public void render(int color, Render2DEvent event) {
-            double[] tickRates = getTickRates();
-            String tickRateString = formatTickRates(tickRates);
-
-            event.getContext().drawTextWithShadow(
-                    mc.textRenderer,
-                    tickRateString,
-                    xCoords.getPlannedValue(),
-                    yCoords.getPlannedValue(),
-                    color
-            );
+        double[] tickRates = getTickRates();
+        String tickRateString = formatTickRates(tickRates);
+        {
+        event.getContext().drawTextWithShadow(
+                mc.textRenderer,
+                tickRateString,
+                xCoords.getPlannedValue(),
+                yCoords.getPlannedValue(),
+                color
+             );
         }
     }
 
